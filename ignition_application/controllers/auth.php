ff5dfdc7e8725af57c12cc1763a590a499ba48d1
Joshua Marketis <josh@clidus.com>
2016-01-30 12:35:12 +0000

    Basic login API for app

--- ignition_application/controllers/auth.php
+++ ignition_application/controllers/auth.php
@@ -14,4 +14,21 @@ require_once APPPATH.'/controllers/ignition/auth.php';
 
 class Auth extends IG_Auth {
 	
+	function appLogin(){
+		$result['error'] = false;
+
+		// read request body and decode into array
+		$requestBody = json_decode(file_get_contents('php://input'),true); 
+
+		// check that username exists
+		if (array_key_exists("username", $requestBody)) {
+		    $result['username'] = $requestBody["username"];
+		} else {
+			$result['error'] = true;
+		}
+
+		// return result
+        echo json_encode($result);
+	}
+
 }
