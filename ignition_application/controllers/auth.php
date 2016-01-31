9b5f027dcf455144f6b9dc1a819b592e7b013018
Joshua Marketis <josh@clidus.com>
2016-01-31 00:54:09 +0000

    Check login credentials

--- ignition_application/controllers/auth.php
+++ ignition_application/controllers/auth.php
@@ -15,18 +15,32 @@ require_once APPPATH.'/controllers/ignition/auth.php';
 class Auth extends IG_Auth {
 	
 	function appLogin(){
-		$result['error'] = false;
+		$result['success'] = true;
 
 		// read request body and decode into array
 		$requestBody = json_decode(file_get_contents('php://input'),true); 
 
-		// check that username exists
-		if (array_key_exists("username", $requestBody)) {
-			$result['username'] = $requestBody["username"];
+		// check that username/password exists in request
+		if (array_key_exists("username", $requestBody) && array_key_exists("password", $requestBody)) {
+			// clean data
+			$this->load->helper('security');
+			$username = trim(xss_clean($requestBody["username"]));
+			$password = trim(xss_clean($requestBody["password"]));
+
+			// send password reset email
+			$this->load->model('User');
+			if(!$this->User->login($username, $password)) {
+				// failed, return error
+				$result['success'] = false;
+				$result['errorMessage'] = "Sorry duder, that seems to be the wrong username or password. Please try again.";
+			}
 		} else {
-			$result['error'] = true;
+			// something has gone very wrong here
+			$result['success'] = false;
+			$result['errorMessage'] = "This request doesn't look right.";
 		}
 
+		// return json response
 		$this->output
 			->set_content_type('application/json')
 			->set_output(json_encode($result));
